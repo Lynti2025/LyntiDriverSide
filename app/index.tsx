6b1b3e52e3042a8115028799eaedbbffffd0f769
varun chaaras <chaarasvarun@gmail.com>
2025-01-26 12:57:55 +0530

    reset

--- app/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from "expo-router";
-import { useState } from "react";
-
-export default function index() {
-  const [isLoggedIn, setisLoggedIn] = useState(false);
-
-  return <Redirect href={!isLoggedIn ? "/signUp" : "/home"} />;
-}
